ccd7fa5ea95f5990f76a8acbed37d7f4668a3948
biu <13073047904@163.com>
2023-11-13 01:33:50 +0800

    exercise sql

---
+++ pymysql_.py
@@ -0,0 +1,13 @@
+import pymysql
+
+connection = pymysql.connect(host='localhost', port=3306, user='root',
+                             password='139391', db='multi_table', charset='utf8')
+
+print(connection)
+cursor = connection.cursor()
+print(cursor)
+with cursor as cursor:
+    sql = 'select * from emp'
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print(result)
